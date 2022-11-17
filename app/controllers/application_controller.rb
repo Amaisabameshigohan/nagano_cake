84da73ba1392eceadd74705f8ae2a2d1a0a3b464
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-11-17 07:54:11 +0000

    [Fix]コントローラーコード削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-#   def after_sign_in_path_for(resource)
-#     new_customer_registration_path
-#     root_path
-#   end
 
-  def after_sign_up_path_for(resource)
-    new_customer_registration_path
-    # root_path
-  end
 
   protected
 
