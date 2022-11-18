9a6cb189485c04988297db84a05ab5e890407235
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-11-18 14:21:24 +0000

    [public-order-new]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,28 @@
 class Public::OrdersController < ApplicationController
+    def new
+      @order = Order.new
+    end
+    
+    def confirm
+        
+    end
+    
+    def complete
+        
+    end
+    
+    def create
+      @order = Order.new(order_params)
+      if @order.save
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
+    
+    private
+    
+    def order_params
+      params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    end
 end
