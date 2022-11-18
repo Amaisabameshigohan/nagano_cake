025fad2b8e93f6d24d53e92f1b446ca1d02f8284
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-11-18 08:09:07 +0000

    [Update]cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,16 @@
 class Public::CartItemsController < ApplicationController
-  before_action :authenticate_custmer!
-  
+  before_action :authenticate_customer!
+
   def index
       @cart_items = current_customer.cart_items.all
   end
-  
+
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
     # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
     #ex.バナナ２個、バナナ２個ではなく　バナナ「4個」にしたい
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-        #元々カート内にあるもの「item_id」　
+        #元々カート内にあるもの「item_id」
         #今追加した　　　params[:cart_item][:item_id])
       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.quantity += params[:cart_item][:quantity].to_i
@@ -18,7 +18,7 @@ class Public::CartItemsController < ApplicationController
                                                           #.to_iとして数字として扱う
       cart_item.save
       redirect_to cart_items_path
-    # もしカート内に「同じ」商品がない場合は通常の保存処理 
+    # もしカート内に「同じ」商品がない場合は通常の保存処理
     elsif @cart_item.save
       @cart_items = current_customer.cart_items.all
       render 'index'
@@ -28,9 +28,9 @@ class Public::CartItemsController < ApplicationController
   end
 
 private
-  
+
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :price, :quantity)
+    params.require(:cart_item).permit(:item_id, :amount)
   end
-    
+
 end
