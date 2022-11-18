2673543f689916169ade07cde1ca8b031a317ced
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-11-18 04:34:27 +0000

    [Update]デザイン

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,7 +6,7 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-
+    @address = Address.find(params[:id])
   end
 
   def create
@@ -16,7 +16,9 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
-
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
