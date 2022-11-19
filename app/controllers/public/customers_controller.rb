1d085a1b5a9cb106fb39678b6d25c336c2652984
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-11-19 03:23:23 +0000

    [Add]controller

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,23 @@
 class Public::CustomersController < ApplicationController
+  
+  def show
+    @customer = current_customer
+  end
+  
+  def edit
+    @customer = current_customer
+  end
+  
+  def unsubscribe
+    @customer = current_customer
+  end
+  
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :address, :telephone_number, :postal_code, :email)
+  end
+
+  
 end
