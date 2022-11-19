beeba1eee66a375328f15d1ad956ec4c5a390a97
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-11-19 03:45:51 +0000

    [Update]機能実装完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,6 +20,26 @@ class Public::CartItemsController < ApplicationController
     end
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    @cart_items = CartItem.all
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    @cart_items = CartItem.all
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    @cart_items = CartItem.all
+    @cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+    
 private
 
   def cart_item_params
