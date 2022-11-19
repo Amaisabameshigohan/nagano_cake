2536eef259c6b904e49492efa12837942c574d7e
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-11-19 14:08:11 +0000

    [Add]編集失敗時のフラッシュメッセージ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,15 +11,28 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = current_customer
     if @customer.update(customer_params)
+      
       redirect_to customers_information_edit_path
     else
-      render "edit"
+      flash[:alert] = "保存できませんでした"
+      redirect_to customers_information_edit_path
     end
   end
+
 
   def unsubscribe
     @customer = current_customer
   end
+  
+  def withdraw
+    @customer = current_customer
+    # @customer.update(is_deleted: true)
+    # reset_session
+    # flash[:notice] = "退会処理を実行いたしました"
+    # redirect_to root_path
+  end
+  
+  
   
   
   private
