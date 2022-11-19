0db414f8863cbefba1124f8bcbe5ec7d2d353f45
EC2 Default User <ec2-user@ip-172-31-29-187.ap-northeast-1.compute.internal>
2022-11-19 03:51:47 +0000

    Add genre search view

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,5 +1,6 @@
 class Public::SearchesController < ApplicationController
   def search
-    @items = Item.all
+    @items = Item.page(params[:page]).search(params[:word])
+    @genres = Genre.all
   end
 end
