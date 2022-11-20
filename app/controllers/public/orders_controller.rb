868a7472324cc574ed788363ae0ffb7e1afb6c84
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-11-20 14:36:57 +0000

    [public-order-confiem]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,28 +1,59 @@
 class Public::OrdersController < ApplicationController
+    def index
+      
+    end
+
     def new
       @order = Order.new
     end
     
-    def confirm
-        
-    end
-    
-    def complete
-        
-    end
-    
     def create
       @order = Order.new(order_params)
-      if @order.save
+      if @order.save!
+        @cart_items = current_customer.cart_items
         redirect_to root_path
       else
         render 'new'
       end
     end
-    
+
+    def confirm
+      @cart_items = current_customer.cart_items
+      @order = Order.new(order_params)
+      @order.payment_method = params[:order][:payment_method]
+      @order.shipping_cost = 800
+      
+      if params[:order][:select_address] == "0"
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name
+      elsif params[:order][:select_address] == "1"
+        @address = Address.find(params[:order][:address_id])
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.name = @address.name
+      elsif params[:order][:select_address] == "2"
+        address_new = current_customer.address.new(address_params)
+        if address_new.save
+        else
+          render :new
+        end
+      else
+        render :new
+      end
+    end
+
+    def complete
+      
+    end
+
+    def show
+      @order = Order.find(params[:id])
+    end
+
     private
-    
+
     def order_params
-      params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+      params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_145837) do
+ActiveRecord::Schema.define(version: 2022_11_19_084850) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_11_18_145837) do
     t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -99,8 +100,10 @@ ActiveRecord::Schema.define(version: 2022_11_18_145837) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.integer "genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
   create_table "order_details", force: :cascade do |t|
@@ -125,4 +128,5 @@ ActiveRecord::Schema.define(version: 2022_11_18_145837) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "genres"
 end
