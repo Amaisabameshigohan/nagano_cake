78e1b798ccce16453815a2a8e4fec62637f73da4
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-11-21 14:35:13 +0000

    [Start]admin/customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,10 +5,22 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :email
+    validates :encrypted_password
+    validates :postal_code
+    validates :address
+    validates :telephone_number
+  end
+
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
-         
+
 end
