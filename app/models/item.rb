bdf880de4416199130ea3e2ee0a73002c229ecee
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-11-22 16:41:30 +0000

    [Add]注文確定したら注文詳細へ保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items
-
+  has_many :order_details
   has_one_attached :image
   enum is_active: { sale: true, stop_selling: false }
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,6 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+  
+  enum status: { 入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済:4}
 end
