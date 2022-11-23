49355c4b985f326eebf8a3b3b1ceee223fec4ef3
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-11-23 09:02:52 +0000

    [Add]フラッシュメッセージ追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -12,23 +12,30 @@ class Public::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
-    @address.save
-    redirect_to addresses_path
+    if @address.save
+      flash[:notice] = "配送先を追加しました"
+      redirect_to addresses_path
+    else
+      flash[:notice] = "保存できませんでした"
+      redirect_to addresses_path
+    end
   end
 
   def update
     @address = Address.find(params[:id])
     if @address.update(address_params)
-      flash[:alert] = "配送先を追加しました"
+      flash[:notice] = "配送先を変更しました"
       redirect_to addresses_path
+    else
+      flash[:notice] = "保存できませんでした"
+      render :edit
     end
-      flash[:alert] = "保存できませんでした"
-    redirect_to addresses_path
   end
 
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
+    flash[:notice] = "配送先を削除しました"
     redirect_to addresses_path
   end
 
