3ffeae39797bfe74dababcec5bc2fc5c0abe0430
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2022-11-25 04:41:52 +0000

    [修正]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,6 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
     def index
       @orders = current_customer.orders.order(create_at: :desc)
     end
